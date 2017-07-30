b8c6a65dc9140c88000c1381b64c6e0edbcfa73e
dobby77 <dobby77@gmail.com>
2017-07-30 16:07:50 +0900

    js

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var weapons = ["AK47", "Magnum", "Kim jong fat"];
+var soldier = {
+  name: "Nicolas",
+  age: 22,
+  weapon: weapons[2],
+  isKorean: false,
+  isDead: false
+  sayHi: function() {
+    console.log("Hello, my name is" + this.name + "and I'm: " + this.age);
+  }
+};
+
+function
+
+function multiplay(ice, water) {
+  return ice * water;
+}
+
+
+function sum(x,y) {
+  return x + Y;
+}
+
+function substrac(x,y) {
+  return x - Y;
+}
+function divide(x,y) {
+  return x / Y;
+}
+function expo(x,y) {
+  return x**Y;
+}
